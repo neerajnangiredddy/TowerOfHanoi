47be307d14a5b42b18a4e3ea5f122648d3036eab
chimtu <chimtu@localhost.localdomain>
2022-03-27 16:09:55 +0530

    Make it better

---
+++ Box.java
@@ -0,0 +1,36 @@
+
+public class Box {
+
+	int boxID;
+	public Box(int boxID) {
+		this.boxID = boxID;
+	}
+	
+//	public void printBox()
+//	{
+//		int size = boxID;
+//		for(int i=0; i<3; i++)
+//		{
+//			for(int j=0; j<4*size; j++)
+//			{
+//				if(i==0 || i==2)
+//				{
+//					System.out.print("#");
+//				}
+//				else
+//				{
+//					if(j==0 || j==4*size-1)
+//					{
+//						System.out.print("#");
+//					}
+//					else
+//					{
+//						System.out.print(" ");
+//					}
+//				}
+//			}
+//			System.out.println();
+//		}
+//	}
+
+}
